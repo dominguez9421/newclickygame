51cdc32c751637de5a744461115504ca597c1508
Jorge Dominguez <jordom300@gmail.com>
2020-02-14 22:35:44 -0600

    bottom

---
+++ newclickygame/src/components /BottomNavMDC/BottomNavMDC.js
@@ -0,0 +1,9 @@
+import React from "react";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import "./BottomNavMDC.css";
+
+const BottomNavMDC = props => (
+  <BottomNavigation {...props}>{props.children}</BottomNavigation>
+);
+
+export default BottomNavMDC;
