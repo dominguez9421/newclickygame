1b04d89acf77381742e6e9915f01d84494807a43
Jorge Dominguez <jordom300@gmail.com>
2020-02-15 11:02:41 -0600

    Updates

---
+++ precache-manifest.812653a8658719b7b3e0cfc423429730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e333d69acc23d7c9898f98f879beeeeb",
+    "url": "/newclickygame/index.html"
+  },
+  {
+    "revision": "c33a0981ab3307a1960b",
+    "url": "/newclickygame/static/css/main.797abb4e.chunk.css"
+  },
+  {
+    "revision": "c6efe2291ec788ff50b1",
+    "url": "/newclickygame/static/js/2.0d258632.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/newclickygame/static/js/2.0d258632.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c33a0981ab3307a1960b",
+    "url": "/newclickygame/static/js/main.a1fc88cd.chunk.js"
+  },
+  {
+    "revision": "577c4fd613d8242e8347",
+    "url": "/newclickygame/static/js/runtime-main.fee86d98.js"
+  }
+]);
